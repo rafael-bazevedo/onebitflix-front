5d4a18b10f2693c403c8b06a75217690d7924e4c
Rafael Azevedo <rafael.bimbatto.azevedo@gmail.com>
2024-08-28 16:24:14 -0300

    Criação dos elementos, do visual e também da sua responsividade - Seção de cards, homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss";
 import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
 import PresentationSection from "@/src/components/homeNoAuth/presentationSection";
+import CardsSection from "@/src/components/homeNoAuth/cardsSection";
 
 const HomeNotAuth = function () {
   return (
@@ -17,6 +18,7 @@ const HomeNotAuth = function () {
           <HeaderNoAuth />
           <PresentationSection />
         </div>
+        <CardsSection />
       </main>
 		</>
   )
