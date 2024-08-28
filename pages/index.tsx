be532b9cc66eb3fecd71c7dcf0265fcdf3ce0f6d
Rafael Azevedo <rafael.bimbatto.azevedo@gmail.com>
2024-08-28 13:48:41 -0300

    Criação do header da página homeNoAuth - Header Não autenticado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss";
+import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
 
 const HomeNotAuth = function () {
   return (
 		<>
 			<Head>
-        <title>Home</title>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+        <meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."></meta>
       </Head>
-			<main></main>
+			<main>
+        <HeaderNoAuth />
+      </main>
 		</>
   )
 };
