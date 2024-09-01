a5087662bcdafa5c0b818bde33c9cd7b3647a064
Rafael Azevedo <rafael.bimbatto.azevedo@gmail.com>
2024-08-31 22:27:47 -0300

    Finalizada a seção dos slides, visual e também responsividade - Seção dos slides

--- src/components/common/slideCard/index.tsx
+++ src/components/common/slideCard/index.tsx
@@ -9,7 +9,7 @@ interface props {
 const SlideCard = function ({ course }: props) {
   return (
     <>
-      <Link href={`/courses/${course.id}`}>
+      <Link href={`/courses/${course.id}`} style={{ textDecoration: "none" }}>
         <div className={styles.slide}>
           <img
             src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`}
