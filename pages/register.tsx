f28d1219c3a55e7932f4f9279f2c6f48fad8e9f6
Rafael Azevedo <rafael.bimbatto.azevedo@gmail.com>
2024-09-03 13:09:15 -0300

    Conexão com o backend na parte de registro, criação do toast e configuração das mensagens de erro. - Registro

--- pages/register.tsx
+++ pages/register.tsx
@@ -3,8 +3,52 @@ import Head from "next/head";
 import HeaderGeneric from "@/src/components/common/headerGeneric";
 import { Button, Container, Form, FormGroup, Input, Label } from "reactstrap";
 import Footer from "@/src/components/common/footer";
+import { FormEvent, useState } from "react";
+import authService from "@/src/services/authService";
+import { useRouter } from "next/router";
+import ToastComponent from "@/src/components/common/toast";
 
 const Register = function () {
+  const router = useRouter();
+  const [toastIsOpen, setToastOpen] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
+
+  const handleRegister = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const firstName = formData.get("firstName")!.toString();
+    const lastName = formData.get("lastName")!.toString();
+    const phone = formData.get("phone")!.toString();
+    const birth = formData.get("birth")!.toString();
+    const email = formData.get("email")!.toString();
+    const password = formData.get("password")!.toString();
+    const confirmPassword = formData.get("confirmPassword")!.toString();
+    const params = { firstName, lastName, phone, birth, email, password };
+
+    if (password != confirmPassword) {
+      setToastOpen(true);
+      setTimeout(() => {
+        setToastOpen(false);
+      }, 1000 * 3);
+      setToastMessage("Senha e confirmação diferentes");
+    
+      return;
+    }
+
+    const { data, status } = await authService.register(params);
+
+    if (status === 201) {
+      router.push("/login?registred=true");
+    } else {
+      setToastOpen(true);
+      setTimeout(() => {
+        setToastOpen(false);
+      }, 1000 * 3);
+      setToastMessage(data.message);
+    }
+  };
+  
   return (
     <>
 			<Head>
@@ -20,7 +64,7 @@ const Register = function () {
         />
         <Container className="py-5">
           <p className={styles.formTitle}>Bem-vindo(a) ao OneBitFlix!</p>
-          <Form className={styles.form}>
+          <Form className={styles.form} onSubmit={handleRegister}>
             <p className="text-center">
               <strong>Faça a sua conta!</strong>
             </p>
@@ -129,6 +173,11 @@ const Register = function () {
           </Form>
         </Container>
         <Footer />
+        <ToastComponent 
+          color="bg-danger" 
+          isOpen={toastIsOpen} 
+          message={toastMessage} 
+        />
       </main>
     </>
   );
