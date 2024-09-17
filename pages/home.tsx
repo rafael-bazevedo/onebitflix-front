ee303c6772a29263594aecc7fcf479da704431fd
Rafael Azevedo <rafael.bimbatto.azevedo@gmail.com>
2024-09-17 16:35:30 -0300

    Configuração finalizada da nossa seção de destaque, fazendo conexão com o backend, criando mais um método para o serviço de cursos, criando a sua responsividade - Seção destaque

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,18 +1,23 @@
-import HeaderAuth from "@/src/components/headerAuth";
+import HeaderAuth from "@/src/components/common/headerAuth";
+import FeaturedSection from "@/src/components/homeAuth/featuresSection";
 import Head from "next/head";
 
 const HomeAuth = function () {
-  return (
-    <>
-			<Head>
-        <title>Onebitflix - Home</title>
-        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
-      </Head>
-      <main>
-        <HeaderAuth />
-      </main>
-    </>
-  );
-}
+    return (
+        <>
+            <Head>
+                <title>Onebitflix - Home</title>
+                <link
+                    rel="shortcut icon"
+                    href="/favicon.svg"
+                    type="image/x-icon"
+                />
+            </Head>
+            <main>
+                <FeaturedSection />
+            </main>
+        </>
+    );
+};
 
 export default HomeAuth;
