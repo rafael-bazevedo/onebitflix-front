e2c9663bd114a33d6de8b452c05bc686ac470ed6
Rafael Azevedo <rafael.bimbatto.azevedo@gmail.com>
2024-09-17 18:47:45 -0300

    Criação da página home autenticada, do seu estilo visual e também da sua responsividade completa. Implementação de todas as categorias na página e criação de serviços - Home autenticada

--- pages/home.tsx
+++ pages/home.tsx
@@ -4,6 +4,7 @@ import NewestCategory from "@/src/components/homeAuth/newestCategory";
 import FavoriteCategory from "@/src/components/homeAuth/favoriteCategory";
 import FeaturedCategory from "@/src/components/homeAuth/featuredCategory";
 import ListCategories from "@/src/components/homeAuth/listCategories";
+import Footer from "@/src/components/common/footer";
 
 const HomeAuth = function () {
   return (
@@ -22,6 +23,7 @@ const HomeAuth = function () {
         <FavoriteCategory />
         <FeaturedCategory />
         <ListCategories />
+        <Footer />
       </main>
     </>
   );
