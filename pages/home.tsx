5dbbc353e17e838fb3ad77e41ed605ad3e566905
Rafael Azevedo <rafael.bimbatto.azevedo@gmail.com>
2024-09-17 17:03:08 -0300

    Criação dos elementos da seção de lançamento, do seu estilo visual - Seção lançamento

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,23 +1,24 @@
-import HeaderAuth from "@/src/components/common/headerAuth";
 import FeaturedSection from "@/src/components/homeAuth/featuresSection";
+import NewestCategory from "@/src/components/homeAuth/newestCategory";
 import Head from "next/head";
 
 const HomeAuth = function () {
-    return (
-        <>
-            <Head>
-                <title>Onebitflix - Home</title>
-                <link
-                    rel="shortcut icon"
-                    href="/favicon.svg"
-                    type="image/x-icon"
-                />
-            </Head>
-            <main>
-                <FeaturedSection />
-            </main>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Onebitflix - Home</title>
+        <link
+          rel="shortcut icon"
+          href="/favicon.svg"
+          type="image/x-icon"
+        />
+      </Head>
+      <main>
+        <FeaturedSection />
+        <NewestCategory />
+      </main>
+    </>
+  );
 };
 
 export default HomeAuth;
