1a4de0a7f9c4c35cffd7a6ea8f0a06894af94aed
Rafael Azevedo <rafael.bimbatto.azevedo@gmail.com>
2024-09-19 21:53:36 -0300

    Criação dos elementos de formulário de senha e do seu estilo visual - Formulário de senha

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,11 +1,15 @@
 import Head from "next/head";
+import { useState } from "react";
 import styles from "../styles/profile.module.scss";
 import UserForm from "@/src/components/profile/user";
 import HeaderAuth from "@/src/components/common/headerAuth";
 import { Button, Col, Container, Row } from "reactstrap";
 import Footer from "@/src/components/common/footer";
+import PasswordForm from "@/src/components/profile/password";
 
 const UserInfo = function () {
+  const [form, setForm] = useState("userForm");
+
   return (
     <>
       <Head>
@@ -20,15 +24,29 @@ const UserInfo = function () {
           <p className={styles.title}>Minha Conta</p>
           <Row className="pt-3 pb-5">
             <Col md={4} className={styles.btnColumn}>
-              <Button outline className={styles.renderFormBtn}>
+              <Button
+                outline
+                className={styles.renderFormBtn}
+                style={{ color: form === "userForm" ? "#ff0044" : "white" }}
+                onClick={() => {
+                  setForm("userForm");
+                }}
+              >
                 DADOS PESSOAIS
               </Button>
-              <Button outline className={styles.renderFormBtn}>
+              <Button
+                outline
+                className={styles.renderFormBtn}
+                style={{ color: form === "passwordForm" ? "#ff0044" : "white" }}
+                onClick={() => {
+                  setForm("passwordForm");
+                }}
+              >
                 SENHA
               </Button>
             </Col>
             <Col md>
-              <UserForm />
+              {form === "userForm" ? <UserForm /> : <PasswordForm />}
             </Col>
           </Row>
         </Container>
