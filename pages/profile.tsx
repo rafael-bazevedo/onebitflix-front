6642781964ce1529be9d29a18f17d111ef8bc591
Rafael Azevedo <rafael.bimbatto.azevedo@gmail.com>
2024-09-24 21:11:09 -0300

    Criação do método de search, comunicação com todo o front, criação de uma página para o seacrh - search

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -20,7 +20,7 @@ const UserInfo = function () {
         <div className={styles.header}>
           <HeaderAuth />
         </div>
-        <Container className="py-5">
+        <Container className={styles.gridContainer}>
           <p className={styles.title}>Minha Conta</p>
           <Row className="pt-3 pb-5">
             <Col md={4} className={styles.btnColumn}>
